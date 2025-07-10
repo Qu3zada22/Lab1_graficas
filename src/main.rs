d3366b9bcf6c09b3ee305fb6fa32d233ecbbada0
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2025-07-09 23:10:24 -0600

    Framebufferr and linee ready

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod framebuffer;
+mod line;
+
+use raylib::prelude::*;
+use framebuffer::Framebuffer;
+use line::line;
+
 fn main() {
-    println!("Hello, world!");
+    let width = 800;
+    let height = 600;
+
+    let mut framebuffer = Framebuffer::new(width, height, Color::BLACK);
+    framebuffer.set_background_color(Color::new(50, 50, 100, 255));
+    framebuffer.clear();
+
+    framebuffer.set_current_color(Color::GREEN);
+    line(
+        &mut framebuffer,
+        Vector2::new(50.0, 50.0),
+        Vector2::new(350.0, 350.0),
+    );
+
+    framebuffer.set_current_color(Color::RED);
+    line(
+        &mut framebuffer,
+        Vector2::new(350.0, 50.0),
+        Vector2::new(50.0, 350.0),
+    );
+
+    let output_file = "lines.bmp";
+    framebuffer.render_to_file(output_file);
+    println!("Líneas guardadas en {}", output_file);
 }
